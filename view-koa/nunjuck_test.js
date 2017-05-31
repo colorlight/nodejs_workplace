680d0dd4acefd7db4871f6c032aa6f7ca1ae0733
colorlight <colorlightleecaa@gmail.com>
2017-06-01 01:21:08 +0800

    增加了nunjuncks的渲染调试的js，与nunjucks的模板

---
+++ view-koa/nunjuck_test.js
@@ -0,0 +1,43 @@
+const nunjucks = require('nunjucks');
+const fs = require('fs');
+const path = require('path');
+
+var workdir = path.resolve('.');
+var viewspath = path.join(workdir,'static','blog','blogviews');
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape && true,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader(viewspath, {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
+
+
+var s = env.render('view_nunjucks.html');
+
+fs.writeFile('static/blog/blog_nunjuncks.html',s , err => {console.log('finish write file')});
+
